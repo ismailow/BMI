a201fad5731d7d1dcc167f83c30944817c88b332
ismailow <77056380+ismailow@users.noreply.github.com>
2021-06-09 16:39:20 +0500

    finish

--- main.js
+++ main.js
@@ -9,16 +9,22 @@ btn.onclick = function(){
     let bmi = weight / (heightInMetrs * heightInMetrs);
 
     if (bmi <= 16){
-        document.getElementById('result').innerHTML = "Ваш ИМТ: " + bmi.toFixed(1) + ". Выраженный дефицит массы тела"
+        document.getElementById('result').innerHTML = "Ваш ИМТ: " + bmi.toFixed(1) + ". Выраженный дефицит массы тела";
+        result.className = 'danger';
     } else if (bmi <= 18.5){
         document.getElementById('result').innerHTML = "Ваш ИМТ: " + bmi.toFixed(1) + ". Недостаточная масса тела";
+        result.className = 'caution';
     } else if (bmi <= 25){
         document.getElementById('result').innerHTML = "Ваш ИМТ: " + bmi.toFixed(1) + ". Норма";
+        result.className = 'normal';
     } else if (bmi <= 30){
         document.getElementById('result').innerHTML = "Ваш ИМТ: " + bmi.toFixed(1) + ". Ожирение 1 степени";
+        result.className = 'caution';
     } else if (bmi <= 39){
         document.getElementById('result').innerHTML = "Ваш ИМТ: " + bmi.toFixed(1) + ". Ожирение 2 степени";
+        result.className = 'danger';
     } else if (bmi >= 40){
         document.getElementById('result').innerHTML = "Ваш ИМТ: " + bmi.toFixed(1) + ". Ожирение 3 степени";
+        result.className = 'danger';
     };
 };
